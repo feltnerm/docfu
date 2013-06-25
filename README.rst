f3cfabe0e6fa29c15e5fa16c03532fc91ad3890e
Mark Feltner <feltner.mj@gmail.com>
2013-06-25 16:21:38 -0500

    chore: .gitignore, HACKING, LICENSE, README.rst, setup.py
    Add standard python packaging definitions.

---
+++ README.rst
@@ -0,0 +1,18 @@
+=====
+docfu
+=====
+
+docfu makes generating documentation from a git repository easy. You might find it useful if you have a git repository
+and would like to generate documentation on a per-branch or -tag basis.
+
+Usage:
+
+    docfu  --sub-dir "docs/src" "feltnerm/docfu" "/home/feltnerm/public_html/docs"
+
+or
+
+    docfu  --tag "0.1" --sub-dir "docs/src" "feltnerm/docfu" "/home/feltnerm/public_html/docs"
+
+or
+
+    docfu  --branch "develop" --sub-dir "docs/src" "feltnerm/docfu" "/home/feltnerm/public_html/docs"
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+settings = {}
+settings.update(
+    name="docfu",
+    version='0.1dev',
+    author='Mark Feltner',
+    author_email='feltner.mj@gmail.com',
+    license="MIT",
+    url="https://github.com/feltnerm/docfu",
+    packages=['docfu',],
+    data_files=[
+        'README.rst',
+    ],
+    description="Generate static docs from a git repo.",
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Jinja2>=2.7",
+        "Pygments>=1.6",
+        "argparse>=1.2.1",
+        "markdown2>=2.1.0",
+        "smartypants>=1.6.0.3"        
+    ],
+    classifiers=(
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.1',
+        #'Programming Language :: Python :: 3.2', 
+    ),
+    entry_points={
+        'console_scripts': [
+            'docfu = docfu.cli:main',
+        ],     
+    }
+)
+
+setup(**settings)
