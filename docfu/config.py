c87144ca7800b8fc081de76bb93f8c78a17a6b83
Mark Feltner <feltner.mj@gmail.com>
2013-07-03 11:38:11 -0500

    feat: config reader added (not being used, yet)

---
+++ docfu/config.py
@@ -0,0 +1,17 @@
+import os, os.path
+import ConfigParser
+
+class Config(object):
+
+    def __init__(self, config_path=""):
+        self.cp = ConfigParser.ConfigParser()
+        self.section_default = 'docfu'
+        self.config_path = config_path 
+
+    def read(self):
+        self.cp.read(self.config_path)
+
+    def __getattr__(self, name, section=''):
+        if section:
+            return self.cp.get(section, name)
+        return self.cp.get(self.section_default, name)
